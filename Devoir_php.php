f3ce5137090133342598405d176128b825c613c2
ejeanty <51671156+ejeanty@users.noreply.github.com>
2020-01-10 21:07:26 -0500

    Add files via upload

---
+++ Devoir_php.php
@@ -0,0 +1,21 @@
+<?php
+$text= readline("Entrez votre age");
+if ($text >= 55)
+{
+    echo ("Vous etes age");
+} 
+else if($text >= 18)
+{
+    echo ("Vous etes majeur");
+    }
+    else if ($text >= 13)
+    {
+        echo ("Vous etes mineur");
+    }
+    else if (($text >= 3) AND ($text<=12)) 
+    echo ("vous etes un enfant");
+{
+
+}
+       
+?>
